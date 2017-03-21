0accef03ef44e277882ba9ef4478bf582b626aa7
mattfullerseo <mattfullerseo@gmail.com>
2017-03-21 16:59:46 -0400

    added assigment 1

---
+++ Hands On Challenge.js
@@ -0,0 +1,62 @@
+var name = "Hello Matt"
+console.log(name);
+//1
+
+var x = 1125
+var answer = x % 2
+console.log(answer);
+//2
+
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
+//3
+
+var a = true;
+var b = false;
+var c = true;
+console.log(a && b && c)
+//4
+
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2= 10 - (3 * (5/4 + (202 % 3))
+console.log(answer1)
+//5
+
+var num = 12;
+var n = num.toString();
+console.log(n);
+
+var Cheese = "113"
+parseInt(Cheese)
+
+var cheese1 = "1.12356"
+parseFloat(cheese1)
+
+var Hello = "Hello World"
+ParseInt(Hello)
+//6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- wdft_prep_course-master/assignment_1/assignment1.js
+++ wdft_prep_course-master/assignment_1/assignment1.js
@@ -5,11 +5,12 @@ Initialize an empty array below in the variable numbers.
 var numbers;
 console.log('hello world');
 
-
 /*
 Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
+
+
 
 for(var i=0;i<10;i++){
     //use the array push() method here.
